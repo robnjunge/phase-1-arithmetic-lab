a23c800385a7e2316ac6f7cd68da87611228a478
robertnjunge <mubearobert@gmail.com>
2023-07-12 16:07:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = 10;
+
+let num3 = 10;
+let num4 = 6;
+let mod = 10 % 6;
+console.log(mod);
+
+let max = 20;
